9f8f1911196e2e456b435ead58bea54b3929c0c7
bmotadev <bmotadev@gmail.com>
2022-10-04 17:05:07 -0300

    aula 20 - exercicio resolvido 01 concluido

--- Aula20ExercicioResolvido01/Program.cs
+++ Aula20ExercicioResolvido01/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            Console.ReadLine();
+        }
+    }
+}
