8af7e905244fbd9ad5297156f4c8fd867152c532
bmotadev <bmotadev@gmail.com>
2022-10-22 17:35:01 -0300

    aula 49 exercicio resolvido 03 uri 1078

---
+++ Aula49ExercicioResolvido03Uri1078/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercicioresolvido1078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            numero = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i<=10; i++)
+            {
+                int resultado = i * numero;
+                Console.WriteLine(i + " x " + numero + " = " + resultado);
+            }
+        }
+    }
+}
